56c089e39bb03bc06e372fe342096f0136a3c41e
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-07-26 23:24:55 -0300

    error simulation

--- back-end-url-shortener/src/healtcheck/healtcheck.controller.ts
+++ back-end-url-shortener/src/healtcheck/healtcheck.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, } from '@nestjs/common';
+import { Controller, Get, Response } from '@nestjs/common';
 
+import { Response as Res } from 'express';
 
 
 @Controller('healtcheck')
 export class HealtCheckController {
   
   @Get()
-  async healt() {
-    return {
-      status: 'up',
-    };
+  async healt(@Response() response: Res) {
+    const error = false;
+
+    if (error) {
+      return response
+      .status(500)
+      .json({
+          message: 'Erro interno',
+        })
+    }
+
+    return response
+      .status(200)
+      .json({
+          status: 'up',
+        });
   }
 }
